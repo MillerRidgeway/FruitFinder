39dea85b95fa8f6cacddbc8c6446f22ed99cdb9d
Miller Ridgeway <miller.ridgeway@gmail.com>
2020-04-21 10:13:00 -0600

    Got images working

--- loadData.py
+++ loadData.py
@@ -1,14 +1,19 @@
 import os
 import torch
 import numpy as np
+import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader, Dataset, SubsetRandomSampler
 import torchvision  
 from torchvision import transforms, datasets
 
 #Loading data from folder
 def load_data():
-    images_dir = os.path.dirname(os.path.dirname(__file__)) + "/FruitFinder/data/" 
-    fruit_dataset = datasets.ImageFolder(images_dir,transform=transforms.ToTensor())
+    #images_dir = os.path.dirname(os.path.dirname(__file__)) + "/FruitFinder/data/" 
+    transform = transforms.Compose(
+    [transforms.Resize((256,256)),
+     transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])    
+    fruit_dataset = datasets.ImageFolder("data/",transform=transform)
     return fruit_dataset
 
 #Split the dataset into train/test
@@ -21,13 +26,22 @@ def split_data(dataset,valid_percent):
     train_sampler = SubsetRandomSampler(train_idx)
     valid_sampler = SubsetRandomSampler(valid_idx)
 
-    train_loader = torch.utils.data.DataLoader(dataset, 100, train_sampler)
-    valid_loader = torch.utils.data.DataLoader(dataset, 100, valid_sampler)
+    train_loader = torch.utils.data.DataLoader(dataset, 4, train_sampler)
+    valid_loader = torch.utils.data.DataLoader(dataset, 4, valid_sampler)
 
-    for i, (inputs, targets) in enumerate(train_loader):
-        print(i, inputs, targets)
     return train_loader, valid_loader
 
-data = load_data()
-train, test = split_data(data, 20)
-print(train, test)
+def imshow(img):
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+def run():
+    data = load_data()
+    train, test = split_data(data, 20)
+    
+    data_iter = iter(train)
+    images, labels = data_iter.next()
+    imshow(torchvision.utils.make_grid(images))
+
+run()
